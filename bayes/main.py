4a49e28b0f82525430a68ccc4e92d62e1b356583
grzeg <grzegorz.niespodzianek@gmail.com>
2026-01-29 00:20:21 +0100

    bayes

--- bayes/main.py
+++ bayes/main.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 
 class CustomGaussianNB:
@@ -37,6 +37,60 @@ class CustomGaussianNB:
         return self.classes[np.argmax(posteriors)]
 
 
+
+def plot_feature_distribution(X_data, y_data, feat_idx, feature_names, classes, filename):
+    plt.figure(figsize=(10, 6))
+    name = feature_names[feat_idx]
+    for q in classes:
+        subset = X_data[y_data == q, feat_idx]
+        if len(subset) > 0:
+            plt.hist(subset, bins=20, density=True, alpha=0.3, label=f'Jakość {q}')
+    plt.title(f'Rozkład cechy: {name}')
+    plt.xlabel('Wartość')
+    plt.ylabel('Gęstość')
+    plt.legend(prop={'size': 9})
+    plt.tight_layout()
+    plt.savefig(filename, dpi=300)
+    plt.close()
+
+
+def plot_confusion_matrix(y_true, y_pred, classes, title, filename, cmap):
+    cm = confusion_matrix(y_true, y_pred, labels=classes)
+    fig, ax = plt.subplots(figsize=(10, 8))
+    cax = ax.matshow(cm, cmap=cmap, alpha=0.7)
+    fig.colorbar(cax)
+
+    ax.set_xticks(np.arange(len(classes)))
+    ax.set_yticks(np.arange(len(classes)))
+    ax.set_xticklabels(classes)
+    ax.set_yticklabels(classes)
+    plt.xlabel('Przewidziana jakość')
+    plt.ylabel('Rzeczywista jakość')
+    plt.title(title, pad=20)
+
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], 'd'),
+                    ha="center", va="center",
+                    color="black" if cm[i, j] < cm.max() / 2 else "white")
+
+    plt.tight_layout()
+    plt.savefig(filename, dpi=300)
+    plt.close()
+
+
+def calculate_per_class_accuracy(y_true, y_pred, classes):
+    accuracies = []
+    for c in classes:
+        mask = (y_true == c)
+        if np.sum(mask) > 0:
+            acc = accuracy_score(y_true[mask], y_pred[mask])
+            accuracies.append(acc * 100)
+        else:
+            accuracies.append(0.0)
+    return accuracies
+
+
 red_df = pd.read_csv('winequality-red.csv', sep=';')
 white_df = pd.read_csv('winequality-white.csv', sep=';')
 
@@ -59,43 +113,10 @@ model.fit(X_train, y_train)
 y_pred = model.predict(X_val)
 
 all_classes = sorted(np.unique(y))
-
 plt.style.use('ggplot')
 
-
-def plot_feature_distribution(feat_idx, filename):
-    plt.figure(figsize=(10, 6))
-    name = feature_names[feat_idx]
-
-    for q in all_classes:
-        subset = X_train[y_train == q, feat_idx]
-        if len(subset) > 0:
-            plt.hist(subset, bins=20, density=True, alpha=0.3, label=f'Jakość {q}')
-
-    plt.title(f'Rozkład cechy: {name}')
-    plt.xlabel('Wartość')
-    plt.ylabel('Gęstość')
-    plt.legend(prop={'size': 9})
-    plt.tight_layout()
-    plt.savefig(filename, dpi=300)
-    plt.close()
-
-
-plot_feature_distribution(10, 'rozklad_alcohol.png')
-plot_feature_distribution(1, 'rozklad_volatile_acidity.png')
-
-
-def calculate_per_class_accuracy(y_true, y_pred, classes):
-    accuracies = []
-    for c in classes:
-        mask = (y_true == c)
-        if np.sum(mask) > 0:
-            acc = accuracy_score(y_true[mask], y_pred[mask])
-            accuracies.append(acc * 100)
-        else:
-            accuracies.append(0.0)
-    return accuracies
-
+plot_feature_distribution(X_train, y_train, 10, feature_names, all_classes, 'rozklad_alcohol.png')
+plot_feature_distribution(X_train, y_train, 1, feature_names, all_classes, 'rozklad_volatile_acidity.png')
 
 acc_red = calculate_per_class_accuracy(y_val[type_val == 'red'], y_pred[type_val == 'red'], all_classes)
 acc_white = calculate_per_class_accuracy(y_val[type_val == 'white'], y_pred[type_val == 'white'], all_classes)
@@ -106,8 +127,22 @@ fig, ax = plt.subplots(figsize=(12, 6))
 ax.bar(x_indices - width / 2, acc_red, width, label='Wino Czerwone', color='#a52a2a', edgecolor='black')
 ax.bar(x_indices + width / 2, acc_white, width, label='Wino Białe', color='#f4c430', edgecolor='black')
 ax.set_ylabel('Skuteczność (%)')
+ax.set_xlabel('Jakość')
+ax.set_title('Skuteczność klasyfikacji per klasa')
 ax.set_xticks(x_indices)
 ax.set_xticklabels(all_classes)
 ax.legend()
+plt.tight_layout()
 plt.savefig('skutecznosc_pelna.png', dpi=300)
 plt.close()
+
+plot_confusion_matrix(y_val, y_pred, all_classes, 'Macierz Pomyłek - Cały Zbiór', 'macierz_pomylek_calosc.png',
+                      plt.cm.Greens)
+
+mask_red = (type_val == 'red')
+plot_confusion_matrix(y_val[mask_red], y_pred[mask_red], all_classes, 'Macierz Pomyłek - Wino Czerwone',
+                      'macierz_pomylek_red.png', plt.cm.Reds)
+
+mask_white = (type_val == 'white')
+plot_confusion_matrix(y_val[mask_white], y_pred[mask_white], all_classes, 'Macierz Pomyłek - Wino Białe',
+                      'macierz_pomylek_white.png', plt.cm.YlOrBr)
